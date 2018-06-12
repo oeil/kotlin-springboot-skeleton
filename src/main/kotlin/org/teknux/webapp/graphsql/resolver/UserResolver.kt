0f78f50b851fec0cae8f1fb8e707fd2ca44d5ab1
oeil <perso.feyl@gmail.com>
2018-06-12 17:22:16 +0200

    provide arguments on subtypes for query

--- src/main/kotlin/org/teknux/webapp/graphsql/resolver/UserResolver.kt
+++ src/main/kotlin/org/teknux/webapp/graphsql/resolver/UserResolver.kt
@@ -7,7 +7,13 @@ import org.teknux.webapp.service.StoreService
 
 class UserResolver(private val storeService: StoreService): GraphQLResolver<User> {
 
-    fun getClockActions(user: User) : Iterable<ClockAction> {
-        return storeService.getActions(user).orEmpty()
+    fun getClockActions(user: User, type: Int?, containsDesc: String?) : Iterable<ClockAction> {
+        return if (type == null && containsDesc == null) {
+            storeService.getActions(user).orEmpty()
+        } else {
+            storeService.getActions(user).orEmpty().filter { action ->
+                (type?.equals(action.type) ?: true) && (containsDesc?.let { action.desc.contains(it, true)} ?: true)
+            }
+        }
     }
 }
